d682764eef33e8751853496895ea0a23cd253f4a
PauloHenriqueBentodosSantos <phbento@outlook.com.br>
2022-09-14 22:20:48 -0300

    Initial commit

---
+++ app/src/main/java/com/psicoamerican/aula05/Contato.kt
@@ -0,0 +1,36 @@
+package com.psicoamerican.aula05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.psicoamerican.aula05.databinding.ActivityContatoBinding
+
+class Contato : AppCompatActivity() {
+    lateinit var binding: ActivityContatoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityContatoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonIr.setOnClickListener {
+            val msg = """${getString(R.string.nome)}: ${binding.editNome.text}
+            |${getString(R.string.tel)}: ${binding.editTelefone.text}
+            |${getString(R.string.email)}: ${binding.editEmail.text}
+            |
+            |${getString(R.string.contato_tel)}: ${if (binding.checkBoxTel.isChecked) getString(R.string.sim) else getString(R.string.nao) }
+            |${getString(R.string.contato_email)}: ${if (binding.checkBoxEmail.isChecked) getString(R.string.sim) else getString(R.string.nao)}""".trimMargin()
+
+            alert(getString(R.string.Confirmacao), msg)
+        }
+    }
+
+    fun alert(titulo: String, msg: String){
+        AlertDialog.Builder(this)
+            .setTitle(titulo)
+            .setMessage(msg)
+            .setPositiveButton("Ok", null)
+            .create()
+            .show()
+    }
+}
---
+++ app/src/main/java/com/psicoamerican/aula05/Pokemon.kt
@@ -0,0 +1,11 @@
+package com.psicoamerican.aula05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pokemon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon)
+    }
+}
